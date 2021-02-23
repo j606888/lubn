989146b8f178aa194fb52eea38b3e43cb672f842
James <jameslee@kdanmobile.com>
2021-02-23 19:20:54 +0800

    Add unique key to device

--- db/migrate/20210222132005_create_devices.rb
+++ db/migrate/20210222132005_create_devices.rb
@@ -1,7 +1,7 @@
 class CreateDevices < ActiveRecord::Migration[5.2]
   def change
     create_table :devices do |t|
-      t.string :code, null: false
+      t.string :code, unique: true, null: false
       t.string :factory_code, null: false
       t.string :category, null: false
 
